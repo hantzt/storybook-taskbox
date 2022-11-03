a5ccc196bbc9ff6806b434ec6e488b65eacff154
Thibaut Hantz <thibaut.hantz@apizee.com>
2022-11-03 16:57:53 +0100

    "Addons" done

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -33,3 +33,13 @@ Archived.args = {
     state: "TASK_ARCHIVED",
   },
 };
+
+const longString =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at nibh vel lacus interdum porttitor nec quis massa. Aenean finibus enim luctus nunc venenatis euismod. Vestibulum consectetur lobortis lobortis. Integer vulputate non eros id lobortis. Sed vel risus mollis, feugiat velit eu, tincidunt felis. Morbi ultrices porta odio et auctor.";
+export const LongTitle = Template.bind({});
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title: longString,
+  },
+};
